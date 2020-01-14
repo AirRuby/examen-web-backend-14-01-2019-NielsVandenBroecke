db410ba00849e8cc3944396ebb954f7e84d45020
niels vanden broecke <niels.vanden.broecke@hotmail.com>
2020-01-14 13:38:59 +0100

    Add project files.

---
+++ ExamenOnlineGokken/Entities/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamenOnlineGokken.Entities
+{
+    public class Game
+    {
+        public long Id { get; set; }
+        public string Hometeam { get; set; }
+        public string AwayTeam { get; set; }
+        public DateTime DateOfGame { get; set; }
+        public ICollection<Bet> Bets { get; set; }
+    }
+}
